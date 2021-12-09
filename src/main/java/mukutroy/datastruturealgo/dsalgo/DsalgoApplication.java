6f5e27c078f33c1810320a010950ae48bf99a56f
Roy <mukut.roy@capgemini.com>
2021-12-09 21:41:45 +0530

    initial commit

---
+++ src/main/java/mukutroy/datastruturealgo/dsalgo/DsalgoApplication.java
@@ -0,0 +1,13 @@
+package mukutroy.datastruturealgo.dsalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsalgoApplication.class, args);
+	}
+
+}
